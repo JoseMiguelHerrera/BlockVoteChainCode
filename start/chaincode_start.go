d9c59e9b06b9e23d40218e40f32794a2958fb86a
Jose Herrera <jmhm2012@gmail.com>
2016-12-29 18:14:02 -0700

    testing out a more complex data model in the chaincode

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -3,18 +3,20 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
-	"strconv"
 	"strings"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
 
 type Referendum struct { //jose: this struct perhaps should be used in the data model. Right now it's all a flat key,val store.
-	referendumName string
-	noVotes        int
-	yesVotes       int
+	ReferendumName       string
+	ParentReferendumName string
+	BlockchainID         string
+	NoVotes              int
+	YesVotes             int
 }
 
 // SimpleChaincode example simple Chaincode implementation
@@ -37,28 +39,30 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 		return nil, errors.New("Incorrect number of arguments. Expecting 1")
 	}
 
-	/* //for data future model expansion
-	newRef := Referendum{referendumName: args[0], noVotes: 0, yesVotes: 0}
-	jsonRef, err := json.Marshal(newRef)
+	//create data model
+	electionMetaData := &Referendum{ReferendumName: args[0], ParentReferendumName: args[1], NoVotes: 0, YesVotes: 0} //golang struct
+	electionMetaDataJSON, err := json.Marshal(electionMetaData)                                                      //golang JSON (byte array)
+	if err != nil {
+		return nil, err
+	}
+
+	err = stub.PutState("electionMetaData", electionMetaDataJSON) //writes the key-value pair (electionMetaData, json object)
+	if err != nil {
+		return nil, err
+	}
+	/*
+		yesVotes := strconv.Itoa(0)
+		noVotes := strconv.Itoa(0)
+		err = stub.PutState("noVotes", []byte(noVotes)) //initializes a key-value pair (election, "election name")
+		if err != nil {
+			return nil, err
+		}
+
+		err = stub.PutState("yesVotes", []byte(yesVotes)) //initializes a key-value pair (election, "election name")
+		if err != nil {
+			return nil, err
+		}
 	*/
-
-	err := stub.PutState("election", []byte(args[0])) //initializes a key-value pair (election, "election name")
-	if err != nil {
-		return nil, err
-	}
-
-	yesVotes := strconv.Itoa(0)
-	noVotes := strconv.Itoa(0)
-	err = stub.PutState("noVotes", []byte(noVotes)) //initializes a key-value pair (election, "election name")
-	if err != nil {
-		return nil, err
-	}
-
-	err = stub.PutState("yesVotes", []byte(yesVotes)) //initializes a key-value pair (election, "election name")
-	if err != nil {
-		return nil, err
-	}
-
 	return nil, nil
 }
 
@@ -106,37 +110,67 @@ func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string)
 		return nil, errors.New("already voted")
 	}
 
-	yesVotesBytes, err := stub.GetState("yesVotes") //gets value for the given key
-	if err != nil {
-		return nil, err
-	}
-	noVotesBytes, err := stub.GetState("noVotes") //gets value for the given key
-	if err != nil {
+	metadataRaw, err := stub.GetState("electionMetaData")
+	if err != nil { //get state error
 		return nil, err
 	}
 
-	noVotes, err := strconv.Atoi(string(noVotesBytes))
-	yesVotes, err := strconv.Atoi(string(yesVotesBytes))
+	var metaDataStructToUpdate Referendum
+	err = json.Unmarshal(metadataRaw, &metaDataStructToUpdate)
+	if err != nil { //unmarshalling error
+		return nil, err
+	}
 
 	if strings.TrimRight(value, "\n") == "yes" {
-		yesVotes++
-
-		err = stub.PutState("yesVotes", []byte(strconv.Itoa(yesVotes))) //initializes a key-value pair (election, "election name")
-		if err != nil {
-			return nil, err
-		}
+		metaDataStructToUpdate.YesVotes++
 
 	} else if strings.TrimRight(value, "\n") == "no" {
-		noVotes++
-		err = stub.PutState("noVotes", []byte(strconv.Itoa(noVotes))) //initializes a key-value pair (election, "election name")
+		metaDataStructToUpdate.NoVotes++
+	}
+
+	electionMetaDataJSON, err := json.Marshal(metaDataStructToUpdate) //golang JSON (byte array)
+	if err != nil {                                                   //marshall error
+		return nil, err
+	}
+
+	err = stub.PutState("electionMetaData", electionMetaDataJSON) //writes the key-value pair (electionMetaData, json object)
+	if err != nil {
+		return nil, err
+	}
+
+	/*
+		yesVotesBytes, err := stub.GetState("yesVotes") //gets value for the given key
 		if err != nil {
 			return nil, err
 		}
-	}
+		noVotesBytes, err := stub.GetState("noVotes") //gets value for the given key
+		if err != nil {
+			return nil, err
+		}
+
+		noVotes, err := strconv.Atoi(string(noVotesBytes))
+		yesVotes, err := strconv.Atoi(string(yesVotesBytes))
+
+		if strings.TrimRight(value, "\n") == "yes" {
+			yesVotes++
+
+			err = stub.PutState("yesVotes", []byte(strconv.Itoa(yesVotes))) //initializes a key-value pair (election, "election name")
+			if err != nil {
+				return nil, err
+			}
+
+		} else if strings.TrimRight(value, "\n") == "no" {
+			noVotes++
+			err = stub.PutState("noVotes", []byte(strconv.Itoa(noVotes))) //initializes a key-value pair (election, "election name")
+			if err != nil {
+				return nil, err
+			}
+		}
+
+	*/
 
 	err = stub.PutState(name, []byte(value)) //JOSE: writes a key-value pair with the given key and value paramenters. We need to introduce a more complex data model that includes an increasing vote ID, for iterating over votes.
-
-	if err != nil {
+	if err != nil {                          //putstate error
 		return nil, err
 	}
 
